e0494128853e81d2d608f940ec57eedd897d37bc
xSchwarzinger <mathiasschwarzinger24@gmail.com>
2026-04-08 13:42:32 +0200

    verze 0.1
    První verze, vytvořili jsme up_down couter, který počítá od 0 do 255 a pak zpátky do 0

---
+++ PWC_Breathing/PWC_Breathing.srcs/sources_1/new/pwm_module.vhd
@@ -0,0 +1,16 @@
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity pwm_module is
+    Port ( brgh : in STD_LOGIC_VECTOR (7 downto 0);
+           led_on_off : out STD_LOGIC);
+end pwm_module;
+
+architecture Behavioral of pwm_module is
+
+begin
+
+
+end Behavioral;
